2a4a6e40f62787af95579e6c4f45f511a151a964
aswea002 <asweatt18@gmail.com>
2019-07-12 15:00:29 -0400

    getting scraped articles

---
+++ server.js
@@ -0,0 +1,95 @@
+var express = require("express");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+
+
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+
+var db = require("./models");
+
+var PORT = 3000;
+
+
+var app = express();
+
+
+
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost/webscraper", { useNewUrlParser: true });
+
+
+app.get("/scrape", function(req, res) {
+  axios.get("https://old.reddit.com/").then(function(response) {
+    var $ = cheerio.load(response.data);
+    console.log("hello world");
+    // console.log(response.data);
+
+    $(".entry").each(function(i, element) {
+      var result = {};
+    //   console.log(element);
+
+      result.title = $(this)
+        .find("a.title")
+        .text();
+        
+      result.link = $(this)
+        .find("a.title")
+        .attr("href");
+        console.log(result);
+
+      db.Article.create(result)
+        .then(function(dbArticle) {
+          console.log(dbArticle);
+        })
+        .catch(function(err) {
+          console.log(err);
+        });
+    });
+
+    res.send("Scrape Complete");
+  });
+});
+
+app.get("/articles", function(req, res) {
+  db.Article.find({})
+    .then(function(dbArticle) {
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      res.json(err);
+    });
+});
+
+app.get("/articles/:id", function(req, res) {
+  db.Article.findOne({ _id: req.params.id })
+    .populate("note")
+    .then(function(dbArticle) {
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      res.json(err);
+    });
+});
+
+app.post("/articles/:id", function(req, res) {
+  db.Note.create(req.body)
+    .then(function(dbNote) {
+      return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+    })
+    .then(function(dbArticle) {
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      res.json(err);
+    });
+});
+
+app.listen(PORT, function() {
+  console.log("App running on port " + PORT + "!");
+});
